86bf2760263ddc3bc7d66ac73e0bfb5172807b93
Josh Brandoff <josh@joshua.is>
2014-10-26 11:16:23 -0400

    Fix formatting; changed tile server to MapQuest Aerial

--- js/frontend.js
+++ js/frontend.js
@@ -1,239 +1,199 @@
-$(function () {
+$(function() {
 
-	var countriesList = [],
-		locations = [],
-		markers = [],
-		legend = {},
-		rotation;
+    var countriesList = [],
+        locations = [],
+        markers = [],
+        legend = {},
+        rotation;
 
-	var currentInfo = $('#side-menu ul'),
-		body = $('body');
+    var currentInfo = $('#side-menu ul'),
+        body = $('body');
 
-	var earth = new WE.map('earth_div',{
-		sky: true,
-		atmosphere : true
-	});
-
-
-	// This line adds support for OpenStreetMap tiles
-
-	WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-		attribution: '© OpenStreetMap contributors'
-	}).addTo(earth);
+    var earth = new WE.map('earth_div', {
+        sky: true,
+        atmosphere: true
+    });
 
 
-	// Start a rotation animation
-
-	rotation = setInterval(function() {
-
-		var c = earth.getPosition();
-
-		if(c[0] && c[1]){
-			earth.setCenter([c[0], c[1] + 0.1]);
-		}
-
-	}, 30);
+    // This line adds support for MapQuest Aerial tiles
+    WE.tileLayer('http://otile2.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
+        attribution: 'Tiles Courtesy of MapQuest.'
+    }).addTo(earth);
 
 
-	// Opening and closing the side menu
-
-	$('#open-side-menu').on('click', function () {
-		body.toggleClass('open');
-	});
-
-
-	// Get data for the globe from /online
-	// Once initially, and then every 30 sec
-
-	getData();
-	setInterval(getData, 1000*30);
+    // Start a rotation animation
+    rotation = setInterval(function() {
+        var c = earth.getPosition();
+        if (c[0] && c[1]) {
+            earth.setCenter([c[0], c[1] + 0.1]);
+        }
+    }, 30);
 
 
-	// This function sends an ajax get request to /online,
-	// fetches a list of countries and coordinates
-	// then renders the globe
+    // Opening and closing the side menu
 
-	function getData() {
-		$.ajax({
-			url:    '/online',
-			success: function(data) {
-				countriesList = data.countriesList;
-				locations = data.coordinates;
-				render();
-			}
-		});
-	}
+    $('#open-side-menu').on('click', function() {
+        body.toggleClass('open');
+    });
+
+
+    // Get data for the globe from /online
+    // Once initially, and then every 30 sec
+    getData();
+    setInterval(getData, 5000);
+
+
+    // This function sends an ajax get request to /online,
+    // fetches a list of countries and coordinates
+    // then renders the globe
+    function getData() {
+        $.ajax({
+            url: '/online',
+            success: function(data) {
+                countriesList = data.countriesList;
+                locations = data.coordinates;
+                render();
+            }
+        });
+    }
 
     // This function handles everything related to updating the HTML
     // with the returned data from the AJAX call:
+    function render() {
+        var entry;
+        if (!countriesList.length) {
 
-	function render() {
+            // If there isn't anybody online
+            // Show an appropriate message and empty the summary and the list section
+            currentInfo.empty();
+            $('#no-members').show();
+            $('.summary').html('');
 
-		var entry;
+        } else {
+            // If there are people online:
+            // On every call clear the side-menu list and globe markers and fill them up with new info.
+            currentInfo.empty();
+            $('#no-members').hide();
+            for (var i = 0; i < countriesList.length; i++) {
 
-		if (!countriesList.length){
+                // Assign the country's name a color.
+                generateColorPalette(countriesList[i]['countryName']);
 
-			// If there isn't anybody online
-			// Show an appropriate message and empty the summary and the list section
+                // Populate the side-menu with the countries ordered by members online (12 entries tops).
+                if (i < 12) {
 
-			currentInfo.empty();
-			$('#no-members').show();
-			$('.summary').html('');
+                    currentInfo.append($('<li id="country' + (i + 1) + '" class="entry"></li>'));
+                    entry = $('#country' + (i + 1));
+                    entry.append($('<span class="color-effect"></span>').css('background-color', legend[countriesList[i]['countryName']]));
+                    entry.append($('<div class="countryName">' + countriesList[i]['countryName'] + '</div>'));
+                    entry.append($('<span class="count">' + countriesList[i]['usersOnline'] + '</span>'));
+                }
 
-		}
-		else {
+            }
 
-			// If there are people online:
-			// On every call clear the side-menu list and globe markers and fill them up with new info.
-
-			currentInfo.empty();
-
-			$('#no-members').hide();
-
-			for (var i = 0; i < countriesList.length; i++) {
-
-				// Assign the country's name a color.
-
-				generateColorPalette(countriesList[i]['countryName']);
-
-				// Populate the side-menu with the countries ordered by members online (12 entries tops).
-
-				if (i < 12) {
-
-					currentInfo.append($('<li id="country' + (i + 1) + '" class="entry"></li>'));
-
-					entry = $('#country' + (i + 1));
-
-					entry.append($('<span class="color-effect"></span>').css('background-color', legend[countriesList[i]['countryName']]));
-					entry.append($('<div class="countryName">' + countriesList[i]['countryName'] + '</div>'));
-					entry.append($('<span class="count">' + countriesList[i]['usersOnline'] + '</span>'));
-
-				}
-
-			}
-
-			// Summary information
-
-			$('.summary').html(locations.length + ' online <span>/</span> ' + countriesList.length + ' countries');
-
-		}
+            // Summary information
+            $('.summary').html(locations.length + ' online <span>/</span> ' + countriesList.length + ' countries');
+        }
 
 
-		// Calculate number of side-menu entries depending on screen size.
-
-		showEntries();
-
-		$( window ).resize(showEntries);
+        // Calculate number of side-menu entries depending on screen size.
+        showEntries();
+        $(window).resize(showEntries);
 
 
-		// Adding the markers to the globe.
+        // Adding the markers to the globe.
 
-		// First remove all existing markers from the globe and clear the array they are stored in.
-		markers.forEach(function (b) {
-			earth.removeMarker(b);
-		});
-
-		markers = [];
-
-
-		// For each ip location create a new marker
-		locations.forEach(function (a) {
-
-			// Push the new markers to the markers[] array and add them to the globe, adding a popup too.
-			markers.push(WE.marker([a.latitude, a.longitude]).addTo(earth).bindPopup("This user has logged in from <b>" + a.country + '</b>', {maxWidth: 150,maxHeight:100, closeButton: true}));
-
-			// Color the markers depending on the country they are in.
-			$(markers[markers.length-1].element.firstChild).css('background', legend[a.country]);
-
-		});
+        // First remove all existing markers from the globe and clear the array they are stored in.
+        markers.forEach(function(b) {
+            earth.removeMarker(b);
+        });
+        markers = [];
 
 
-		// Rotate the globe to the desired country from the list
+        // For each ip location create a new marker
+        locations.forEach(function(a) {
 
-		$('.entry').on('click', function () {
+            // Push the new markers to the markers[] array and add them to the globe, adding a popup too.
+            markers.push(WE.marker([a.latitude, a.longitude]).addTo(earth).bindPopup("This user has logged in from <b>" + a.country + '</b>', {
+                maxWidth: 150,
+                maxHeight: 100,
+                closeButton: true
+            }));
 
-			if($(window).width()<800){
-				body.removeClass('open');
-			}
-
-			clearInterval(rotation);
-
-			var index = $(this).attr('id').split('country')[1] - 1;
-
-			earth.panTo([countriesList[index]['latitude'], countriesList[index]['longitude']]);
-
-		});
-
-	}
+            // Color the markers depending on the country they are in.
+            $(markers[markers.length - 1].element.firstChild).css('background', legend[a.country]);
+        });
 
 
-	$('#earth_div').on('click touchstart touchend', function () {
+        // Rotate the globe to the desired country from the list
+        $('.entry').on('click', function() {
+            if ($(window).width() < 800) {
+                body.removeClass('open');
+            }
+            clearInterval(rotation);
+            var index = $(this).attr('id').split('country')[1] - 1;
+            earth.panTo([countriesList[index]['latitude'], countriesList[index]['longitude']]);
+        });
 
-		// Stop the rotation when the earth is clicked or touched
-		clearInterval(rotation);
+    }
 
 
-		// If the screen is small close the side menu as well.
-		if($(window).width()<800){
-			body.removeClass('open');
-		}
+    $('#earth_div').on('click touchstart touchend', function() {
+        // Stop the rotation when the earth is clicked or touched
+        clearInterval(rotation);
 
-	});
+        // If the screen is small close the side menu as well.
+        if ($(window).width() < 800) {
+            body.removeClass('open');
+        }
+    });
 
 
-	// Shows only a part of the entries depending on the screen size
-
-	function showEntries() {
-
-		var screenHeight = $(window).height(),
-			sideMenuEntriesSpace = screenHeight - 250, // Exclude the title and the summary height
-			numberOfEntries = Math.round(sideMenuEntriesSpace/55); // 55 is the height of an entry in pixels
+    // Shows only a part of the entries depending on the screen size
+    function showEntries() {
+        var screenHeight = $(window).height(),
+            sideMenuEntriesSpace = screenHeight - 250, // Exclude the title and the summary height
+            numberOfEntries = Math.round(sideMenuEntriesSpace / 55); // 55 is the height of an entry in pixels
 
         // The list of countries on the sidebar
-		var countries = $('#side-menu .entry');
+        var countries = $('#side-menu .entry');
 
         // Hide all countries
-		countries.hide();
+        countries.hide();
 
         // Show only as many countries as would fit on the window
-
-		countries.slice(0, numberOfEntries).show();
-
-	}
+        countries.slice(0, numberOfEntries).show();
+    }
 
 
-	var j=0;
+    var j = 0;
 
     var arrayOfColors = [
-        '#25c19e','#c354f6','#7a54f6','#2593c1','#f68954','#5cc151',
-        '#db3095','#c52a82','#d7c834','#e24912','#e3c44a','#d630ae'
+        '#25c19e', '#c354f6', '#7a54f6', '#2593c1', '#f68954', '#5cc151',
+        '#db3095', '#c52a82', '#d7c834', '#e24912', '#e3c44a', '#d630ae'
     ];
 
 
-	function generateColorPalette(name) {
+    function generateColorPalette(name) {
+        // Fill up a legend object with the key a country name
+        // and the value a color taken from colors[]
+        if (!legend[name]) {
+            legend[name] = arrayOfColors[j];
+        }
 
-		// Fill up a legend object with the key a country name
-		// and the value a color taken from colors[]
+        j++;
 
-		if(!legend[name]){
-			legend[name] = arrayOfColors[j];
-		}
-
-		j++;
-
-		if (j >= arrayOfColors.length) {
-			j = 0;
-		}
-	}
+        if (j >= arrayOfColors.length) {
+            j = 0;
+        }
+    }
 
 
-	// Open the side menu on start up if the app is running on a big screen.
-
-	if($(window).width()>800){
-		body.addClass('open');
-	}
-	else {
-		earth.setZoom(1.8);
-	}
+    // Open the side menu on start up if the app is running on a big screen.
+    if ($(window).width() > 800) {
+        body.addClass('open');
+    } else {
+        earth.setZoom(1.8);
+    }
 
 });

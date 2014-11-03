0b772f4be2f5f4f01818e0cf700501c6b84d1a4d
Josh Brandoff <josh@joshua.is>
2014-11-03 10:25:15 -0500

    Specify subdomains.

--- js/frontend.js
+++ js/frontend.js
@@ -17,7 +17,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 
     // This line adds support for MapQuest Aerial tiles
-    WE.tileLayer('https://otile2-s.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
+    WE.tileLayer('https://otile{s}-s.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
+        subdomains: '1234',
         attribution: 'Tiles Courtesy of MapQuest.'
     }).addTo(earth);
 
